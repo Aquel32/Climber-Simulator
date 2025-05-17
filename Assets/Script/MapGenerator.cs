d80a951f0d46c6bd7f1bd804d5a2189c279c6f14
Aquel32 <aquelyt@gmail.com>
2025-05-17 16:48:58 +0200

    Coloring based on height

--- Assets/Script/MapGenerator.cs
+++ Assets/Script/MapGenerator.cs
@@ -2,24 +2,37 @@ using NUnit.Framework.Internal;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using Unity.Mathematics;
 using UnityEngine;
 using static UnityEditor.PlayerSettings;
 
 public class MapGenerator : MonoBehaviour
 {
     [Header("Settings")]
-    [SerializeField] private int width;
-    [SerializeField] private int height;
+    [SerializeField] private int mapWidth;
+    [SerializeField] private int mapHeight;
     [SerializeField] private float scale;
     [SerializeField] private float factor;
+    [SerializeField] private int seed;
 
     [Header("Mesh Rendering")]
     [SerializeField] private MeshFilter meshFilter;
-    [SerializeField] private Gradient gradient;
     private Mesh mesh;
     private List<Vector3> vertices;
     private List<int> triangles;
     private Color[] colors;
+
+    [System.Serializable]
+    public struct MapRegion
+    {
+        public string name;
+        public float height;
+        public Color color;
+    };
+
+    [SerializeField] private MapRegion[] regions;
+
+    private System.Random random;
 
     void Start()
     {
@@ -34,31 +47,44 @@ public class MapGenerator : MonoBehaviour
     {
         vertices = new List<Vector3>();
         triangles = new List<int>();
-        colors = new Color[width * height];
+        colors = new Color[mapWidth * mapHeight];
 
-        for (float y = 0, i = 0; y < height; y++)
+        for (float y = 0, i = 0; y < mapHeight; y++)
         {
-            for (float x = 0; x < width; x++)
+            for (float x = 0; x < mapWidth; x++)
             {
-                float elevation = Mathf.PerlinNoise(x * factor + 0.1f, y * factor + 0.1f);
+                float height = GetHeight(x, y);
+                vertices.Add(new Vector3(x - (mapWidth/2), (height * scale) - (scale / 2), y - (mapHeight/2)));
 
-                colors[(int)i] = gradient.Evaluate(elevation);
-                vertices.Add(new Vector3(x - (width/2), elevation * scale, y - (height/2)));
-
-                if (x < width - 1 && y < height - 1)
+                if (x < mapWidth - 1 && y < mapHeight - 1)
                 {
-                    triangles.Add((int)i + width);
-                    triangles.Add((int)i + width + 1);
+                    triangles.Add((int)i + mapWidth);
+                    triangles.Add((int)i + mapWidth + 1);
                     triangles.Add((int)i);
 
                     triangles.Add((int)i + 1);
                     triangles.Add((int)i);
-                    triangles.Add((int)i + width + 1);
+                    triangles.Add((int)i + mapWidth + 1);
+                }
+
+                for (int regionIndex = 0; regionIndex < regions.Length; regionIndex++)
+                {
+                    if (height < regions[regionIndex].height)
+                    {
+                        colors[(int)i] = regions[regionIndex].color;
+                    }
                 }
 
                 i++;
             }
         }
+    }
+
+    public float GetHeight(float x, float y)
+    {
+        float height = Mathf.PerlinNoise(x / factor + seed, y / factor + seed);
+
+        return height;
     }
 
     public void UpdateMesh()
